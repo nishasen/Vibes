b76672a6a50eb1697091ed7da390c8625ce04c91
nishasen <43262505+nishasen@users.noreply.github.com>
2022-05-10 12:32:12 +0530

    Added routes and data of videos

--- vibes/src/App.js
+++ vibes/src/App.js
@@ -1,17 +1,30 @@
 import "./App.css";
 import MockAPI from './Mockman/Mockman';
 import { Routes, Route } from 'react-router-dom';
-
+import { Home, Login, Signup, Explore, SingleVideo, History, Liked, Playlist, PlaylistListing, WatchLater } from './Pages';
+import { RequireAuth } from "./Components";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+ 
 function App() {
   return (
     <div className="App">
-     Hello world
-     <Routes>
-      <Route path="/mockman" element={<MockAPI/>} />
-     </Routes>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mockman" element={<MockAPI/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/explore/video/:id" element={<SingleVideo/>} />
+        <Route path="/history" element={<RequireAuth><History/></RequireAuth>} />
+        <Route path="/liked" element={<RequireAuth><Liked /></RequireAuth>} />
+        <Route path="/playlist" element={<RequireAuth><Playlist/></RequireAuth>} />
+        <Route path="/playlist/:id" element={<RequireAuth><PlaylistListing/></RequireAuth>} />
+        <Route path="/watchlater" element={<RequireAuth><WatchLater/></RequireAuth>} />
+      </Routes>
     </div>
   );
 }
-//#03989E
 
 export default App;

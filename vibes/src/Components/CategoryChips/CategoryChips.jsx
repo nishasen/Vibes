1daed07afdb70d33385e25108cd262d457d97909
nishasen <43262505+nishasen@users.noreply.github.com>
2022-05-10 13:39:53 +0530

    Added explore and single video page
    Explore page contains all the videos listed and and single video page contains video player and some video suggestions related to same category

---
+++ vibes/src/Components/CategoryChips/CategoryChips.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Button } from '..';
+import { useCategory, useVideo } from '../../Contexts';
+import style from './CategoryChips.module.css';
+
+const CategoryChips = () => {
+    const {categories} = useCategory();
+    const { videoState, videoDispatch } = useVideo();
+    const { categoryFilter } = videoState;
+  return (
+<div className={`dis-flex ${style.categories}`}>
+        <Button text="All" 
+                contained={categoryFilter==="All"} 
+                onClick={()=>{videoDispatch({type: "CATEGORY_NAME", payload: "All"})}}/>
+        {categories.map(({_id, categoryName}) => <Button text={categoryName} 
+                                                        contained={categoryName===categoryFilter} 
+                                                        onClick={()=>{videoDispatch({type: "CATEGORY_NAME", payload: categoryName})}} 
+                                                        key={_id}/>)}
+    </div>
+  )
+}
+
+export { CategoryChips };
